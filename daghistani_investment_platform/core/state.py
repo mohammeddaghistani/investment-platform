54d9a057f7d6fd16ca2996d96c1519c1e4e807e2
mohammeddaghistani <mdsa2003@gmail.com>
2026-01-17 07:32:52 +0300

    Delete daghistani_investment_platform directory

--- daghistani_investment_platform/core/state.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def ensure_defaults():
-    st.session_state.setdefault("inputs", {})
-    st.session_state.setdefault("results", {})
-    st.session_state.setdefault("portfolio_df", None)
